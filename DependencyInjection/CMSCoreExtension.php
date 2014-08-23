00d5e7fece2903e3ed126f793a74cca970cb71a2
Charlie Hadden <charlie@hadden.me>
2014-08-23 10:05:35 +0100

    Load services.yml

--- DependencyInjection/CMSCoreExtension.php
+++ DependencyInjection/CMSCoreExtension.php
@@ -4,6 +4,8 @@ namespace CMS\CoreBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class CMSCoreExtension extends Extension
 {
@@ -12,5 +14,9 @@ class CMSCoreExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $locator = new FileLocator(__DIR__ . '/../Resources/config');
+        $loader = new YamlFileLoader($container, $locator);
+
+        $loader->load('services.yml');
     }
 }
