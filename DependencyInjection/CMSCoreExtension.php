87b9ea9c06c0a6b043eb35fdd4a2d50a23d03998
Charlie Hadden <charlie@hadden.me>
2014-08-23 09:48:21 +0100

    Get basic boilerplate in place

---
+++ DependencyInjection/CMSCoreExtension.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CMS\CoreBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class CMSCoreExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+}
