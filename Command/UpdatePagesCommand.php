4a903764c45c752d1df84c100d7828fba7452b60
Charlie Hadden <charlie@hadden.me>
2014-08-24 18:00:23 +0100

    Removes unused use statement

--- Command/UpdatePagesCommand.php
+++ Command/UpdatePagesCommand.php
@@ -7,7 +7,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use Symfony\Component\Finder\Finder;
-use Symfony\Component\Finder\SplFileInfo;
 
 use CMS\CoreBundle\Entity\Page;
 use CMS\CoreBundle\Entity\Field;
